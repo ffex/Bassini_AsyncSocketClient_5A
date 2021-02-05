933672311e0d82881f383a591a3f31708bb57e71
ffex <kurogan21@gmail.com>
2021-02-05 10:09:07 +0100

    Update AsyncSocketClient.cs

--- Bassini_SocketAsyncLib/AsyncSocketClient.cs
+++ Bassini_SocketAsyncLib/AsyncSocketClient.cs
@@ -40,6 +40,42 @@ namespace Bassini_SocketAsyncLib
             mServerIpAddress = ipaddr;
             return true;
         }
+        public bool SetServerPort(string str_port)
+        {
+            int port = -1;
+            if (!int.TryParse(str_port,out port))
+            {
+                Console.WriteLine("Porta non valida");
+                return false;
+            }
+            if(port <0 || port > 65535)
+            {
+                Console.WriteLine("La porta deve essere compresa tra 0 e 65535");
+                return false;
+            }
+            mServerPort = port;
+            return true;
+                
+        }
+        public async Task ConnettiAlServer()
+        {
+            if (mClient == null)
+            {
+                mClient = new TcpClient();
+            }
+
+            try
+            {
+                await mClient.ConnectAsync(mServerIpAddress, mServerPort);
+                Console.WriteLine("Connesso al server IP/Port: {0} / {1}",
+                                    mServerIpAddress.ToString(),mServerPort);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
 
         public AsyncSocketClient()
         {
